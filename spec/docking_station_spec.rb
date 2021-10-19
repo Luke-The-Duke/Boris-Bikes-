8287dfe1413a3c2e9e6a43d4c06068164ef635fa
Luke Carney <77110437+Luke-The-Duke@users.noreply.github.com>
2021-10-19 16:12:37 +0100

    step 8

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
 require "docking_station"
 
-describe DockingStation do
+# describe DockingStation do
+#   it "responds to release_bike" do
+#     expect {DockingStation.new.release_bike}.not_to raise_error
+#   end 
+# end    
 
-end    
+describe DockingStation do
+  it { is_expected.to respond_to :release_bike }  
+end   
